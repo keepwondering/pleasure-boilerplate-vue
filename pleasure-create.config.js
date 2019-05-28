64d189b16606148b5b0ad0c24c550c04567ca037
Martin Rafael Gonzalez <tin@devtin.io>
2019-05-28 10:24:44 -0400

    initial

---
+++ pleasure-create.config.js
@@ -0,0 +1,74 @@
+const empty = v => {
+  return /^[\s]*$/.test(v)
+}
+
+const kebabCase = str => {
+  return str
+    .normalize('NFD')
+    // remove diacritics
+    .replace(/[\u0300-\u036f]/g, '')
+    // split string
+    .replace(/([A-Z][a-z]+)/g, '-$1-')
+    .toLowerCase()
+    .replace(/[^a-z0-9-]+/ig, '-')
+    .replace(/-{2,}/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '')
+}
+module.exports = {
+  // see `inquirer.prompt` in https://github.com/SBoudrias/Inquirer.js/
+  prompts: [
+    {
+      name: 'author',
+      message: 'Author name'
+    },
+    {
+      name: 'projectName',
+      message: 'Project name',
+      validate (n) {
+        return empty(n) ? 'Enter a valid project name' : true
+      },
+      filter: kebabCase,
+      transformer (v) {
+        return kebabCase(v)
+      }
+    },
+    {
+      type: 'checkbox',
+      name: 'config',
+      message: 'Distribute for',
+      choices: [
+        {
+          name: 'cjs',
+          value: 'cjs',
+          checked: true
+        },
+        {
+          name: 'esm',
+          value: 'esm',
+          checked: true
+        },
+        {
+          name: 'iife',
+          value: 'iife',
+          checked: false
+        }
+      ]
+    },
+    {
+      name: 'iifeName',
+      message: 'iife module name',
+      when (a) {
+        return a.config.indexOf('iife') >= 0
+      }
+    }
+  ],
+  transform (data) {
+    const config = {}
+    data.config.forEach(v => {
+      config[v] = true
+    })
+    data.config = config
+    return data
+  }
+}
