3fbb50871eec21da8d6537b10a527ee8d2b3bbdc
Martin Rafael Gonzalez <tin@devtin.io>
2019-06-13 17:10:57 -0400

    add postcss

---
+++ postcss.variables.js
@@ -0,0 +1,24 @@
+const colors = {
+  red: '#e23533',
+  green: '#27BAB4',
+  gray: '#999',
+  black: '#343434',
+  sand: '#fef0c1',
+  white: '#e7e7e7'
+}
+
+const palette = {
+  primary: colors.black,
+  light: colors.gray,
+  secondary: colors.green,
+  warning: colors.sand,
+  danger: colors.red,
+  dark: colors.black
+}
+
+module.exports = {
+  myProject: {
+    palette,
+    mainGap: 11
+  }
+}
